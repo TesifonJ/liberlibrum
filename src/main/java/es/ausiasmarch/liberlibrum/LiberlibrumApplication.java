bc385f57dde1d97fe8c5fd4d277a6d3cc6822d49
TesifonJ <coul1212@gmail.com>
2023-10-28 13:45:15 +0200

    first commit

---
+++ src/main/java/es/ausiasmarch/liberlibrum/LiberlibrumApplication.java
@@ -0,0 +1,13 @@
+package es.ausiasmarch.liberlibrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberlibrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberlibrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ausiasmarch/liberlibrum/LiberlibrumApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ausiasmarch.liberlibrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberlibrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
